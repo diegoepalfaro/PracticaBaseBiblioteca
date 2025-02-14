fe691192004495a917fd9b834380eaaa9a79bcce
diegoepalfaro <diegoepalfaro@gmail.com>
2025-02-14 17:27:36 -0600

    Agregar archivos de proyecto.

---
+++ PracticaBaseBiblioteca/Models/BibliotecaContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace PracticaBaseBiblioteca.Models
+{
+    public class BibliotecaContext : DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+        }
+    }
+}
